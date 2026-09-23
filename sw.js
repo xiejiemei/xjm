6bf31b49903c72fb27957fe6970fb3263a01471c
xiejiemei <945551252@qq.com>
2026-09-23 15:41:27 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'qxt-app-v11';
+const ASSETS = ['./', './index.html', './app-192.png', './app-512.png', './manifest.webmanifest'];
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', (e) => { const req = e.request; if (req.method !== 'GET') return; e.respondWith(caches.match(req).then((hit) => hit || fetch(req).then((res) => { const ok = res && res.status === 200 && (res.type === 'basic' || res.type === 'cors'); if (ok) { const cl = res.clone(); caches.open(CACHE).then((c) => c.put(req, cl)); } return res; }).catch(() => caches.match('./index.html')))); });
